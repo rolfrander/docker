bce9a7c524724dc6efc6afb1606e9a724e5f4cc6
Rolf Rander Næss <rolfn@pvv.org>
2020-05-11 18:39:25 +0200

    indent

--- haproxy/letsencrypt/bin/ocsp-staple.sh
+++ haproxy/letsencrypt/bin/ocsp-staple.sh
@@ -3,15 +3,15 @@
 cd "$LIVE_CERT_FOLDER"
 for DIR in *
 do
-    if [ -d "$DIR" ]
-    then
-	# Get the OCSP URL from the certificate
-	ocsp_url=$(openssl x509 -noout -ocsp_uri -in ${DIR}/cert.pem)
+  if [ -d "$DIR" ]
+  then
+  	# Get the OCSP URL from the certificate
+  	ocsp_url=$(openssl x509 -noout -ocsp_uri -in ${DIR}/cert.pem)
 
-	openssl ocsp -noverify -no_nonce \
-		-issuer ${DIR}/chain.pem \
-		-cert ${DIR}/cert.pem \
-		-url $ocsp_url \
-		-respout ${DIR}/cert.pem.ocsp
-    fi
+  	openssl ocsp -noverify -no_nonce \
+            		-issuer ${DIR}/chain.pem \
+            		-cert ${DIR}/cert.pem \
+            		-url $ocsp_url \
+            		-respout ${DIR}/cert.pem.ocsp
+  fi
 done
